637c6b3fc86f51054202cd0a0a2004a918005118
Martin Asnong <martin.asnong@gmail.com>
2023-01-20 15:19:59 -0500

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="xstate-v5-alpha-react-experiment" />
+      <div />
+    </>
+  );
+}
+
+export default App;
